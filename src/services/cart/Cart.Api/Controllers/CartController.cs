866d0a93500c5590cef502b94c24e1253c49fb16
Egor Hotenov <teremok1123@mail.ru>
2026-06-08 22:12:31 +0500

    Какой никакой вяленький скелет Cart

---
+++ src/services/cart/Cart.Api/Controllers/CartController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cart.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CartController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new { message = "Cart API is working!" });
+    }
+}

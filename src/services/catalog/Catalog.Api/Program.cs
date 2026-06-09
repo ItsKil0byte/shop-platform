1f7e5755ae68e826b8ecb6d4332e3b1ef887c19a
Egor Hotenov <teremok1123@mail.ru>
2026-06-09 21:33:49 +0500

    Настройка апи и контекста БД

--- src/services/catalog/Catalog.Api/Program.cs
+++ src/services/catalog/Catalog.Api/Program.cs
@@ -1,7 +1,14 @@
+using Catalog.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<CatalogDbContext>(options => 
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 

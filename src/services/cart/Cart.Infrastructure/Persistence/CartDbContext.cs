2aa39ff7a3e1506cf83afe515004944ac70de11f
Egor Hotenov <teremok1123@mail.ru>
2026-06-10 21:28:28 +0500

    БД корзинко

---
+++ src/services/cart/Cart.Infrastructure/Persistence/CartDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Cart.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cart.Infrastructure.Persistence;
+
+public class CartDbContext : DbContext
+{
+    public CartDbContext(DbContextOptions<CartDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<CartItem> CartItems { get; set; }
+}
